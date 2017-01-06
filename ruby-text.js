d1ba68d9f84bc0978c36f54b81eef9036dc52b0e
jschanker <jschanker@molloy.edu>
2017-01-05 20:14:43 -0500

    Removed Manually Created getBlocks function

--- ruby-text.js
+++ ruby-text.js
@@ -1,24 +1,5 @@
 var textField = document.getElementById("text-code");
 
-function getBlockObjects() {
-  // TODO: Replace with generated JS file
-  
-  var blocks = {};
-  blocks['num_of_minutes'] = {"type":"num_of_minutes","message0":"number of minutes in  %1 .seconds","args0":[{"type":"input_value","name":"SECONDS"}],"inputsInline":true,"output":null,"colour":230,"tooltip":"","helpUrl":"http://www.example.com/"};
-  
-  blocks['num_remaining_seconds_after_minutes'] = {"type":"num_remaining_seconds_after_minutes","message0":"number of remaining seconds from %1 .seconds after minutes removed","args0":[{"type":"input_value","name":"SECONDS","check":"Number"}],"inputsInline":true,"output":null,"colour":230,"tooltip":"","helpUrl":"http://www.example.com/"};
-
-  blocks['num_of_hours_in_minutes'] = {"type":"num_of_hours_in_minutes","message0":"number of hours in %1 .minutes","args0":[{"type":"input_value","name":"MINUTES","check":"Number"}],"output":null,"colour":230,"tooltip":"","helpUrl":"http://www.example.com/"};
-
-  blocks['num_of_days'] = {"type":"num_of_days","message0":"number of days in %1 .hours","args0":[{"type":"input_value","name":"HOURS","check":"Number"}],"output":null,"colour":230,"tooltip":"","helpUrl":"http://www.example.com/"};
-
-  blocks['num_of_remaining_minutes_after_hours_removed'] = {"type":"num_of_remaining_minutes_after_hours_removed","message0":"number of remaining minutes from %1 .minutes after hours removed","args0":[{"type":"input_value","name":"MINUTES","check":"Number"}],"output":null,"colour":230,"tooltip":"","helpUrl":"http://www.example.com/"};
-
-  blocks['num_of_remaining_hours_after_days'] = {"type":"num_of_remaining_hours_after_days","message0":"number of remaining hours from %1 .hours after days removed","args0":[{"type":"input_value","name":"HOURS","check":"Number"}],"output":null,"colour":230,"tooltip":"","helpUrl":"http://www.example.com/"};
-
-  return blocks;
-}
-
 function createText(xmlDoc, root) {
     var blocks = getBlockObjects();
     var text = "";
